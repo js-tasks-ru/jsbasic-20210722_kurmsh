212860aa6c30b6f6db5a293655e0273bf150b07c
Amidala Io <io.amidala@gmail.com>
2021-07-29 19:02:25 +0300

    решение факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  let buffer = n;
+  let result = 0;
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  for (let i = 1; i < n; i++) {
+    buffer = buffer * (n - i);
+  }
+  return result = buffer * 1;
+
 }
