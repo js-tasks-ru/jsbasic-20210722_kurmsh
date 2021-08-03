b5d2120e6f749346e9ec7143fe2157cd33ee4c71
Io-Amidala <io.amidala@gmail.com>
2021-08-03 11:45:44 +0300

    Дописала функцию валидации имени пользователя

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -11,8 +11,24 @@ function print(text) {
  */
 function isValid(name) {
   // ваш код...
-}
 
+  if (name === null || name === '' || name.length < 4) {
+    console.log('поле пустое или содержит меньше 4 символов');
+    return false;
+  }
+
+
+
+  for (let i = 0; i < name.length; i++) {
+    if (name[i] == ' ') {
+      console.log('Поле имеет пробелы');
+      return false;
+    }
+
+  }
+  return true;
+
+}
 function sayHello() {
   let userName = prompt('Введите ваше имя');
 
@@ -22,3 +38,4 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+sayHello();
