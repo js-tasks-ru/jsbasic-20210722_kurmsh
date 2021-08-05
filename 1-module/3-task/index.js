55236c496d820c35a4320e875dac620a7936ee70
Io-Amidala <io.amidala@gmail.com>
2021-08-05 11:36:47 +0300

    Сделать первый символ заглавным

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,19 @@
 function ucFirst(str) {
-  // ваш код...
+  let str1 = str;
+  let target = 'В';
+  let pos = 0;
+  while (true) {
+    let foundPos = str.indexOf(target, pos);
+    if (foundPos == -1) break;
+    console.log(`Найдено тут: ${foundPos}`);
+    pos = foundPos + 1;
+  }
+  if (!str1) return str1;
+  return str1[0].toUpperCase() + str1.slice(1);
+
 }
+
+// console.log(ucFirst('ваВя'));
+
+
+
