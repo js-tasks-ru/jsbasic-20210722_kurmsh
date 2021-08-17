f90f6c21f1ffd3092ceb9bc1c1341611a2017c24
Io-Amidala <io.amidala@gmail.com>
2021-08-17 18:44:31 +0300

    module4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,7 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  let li = document.createElement('li');
+  ul.innerHTML = '\n<li>Artsiom Mezin</li>\n <li>Ilia Kantor</li>\n <li>Christopher Michael</li>\n ';
+  return ul;
 }
+makeFriendsList(friends);
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
-  // ваш код...
-}
+  let table = document;
+  for (let td of table.querySelectorAll('dataset.available')) {
+    let status = td.getAttribute(dataset.available);
+    if (status === 'false') {
+      td.classList.add('unavailable');
+      console.log(td);
+    } else {
+      td.classList.add('available');
+    }
+  }
+  for (let td of table.querySelectorAll('td')) {
+    let hidden = td.getAttribute('dataset.available');
+    if (!hidden) {
+      td.setAttribute('hidden', true);
+    }
+  }
+  for (let td of table.querySelectorAll('td')) {
+    if (td.textContent === 'm') {
+      td.classList.add('male');
+    } else if (td.textContent === 'f') {
+      td.classList.add('female');
+    }
+  }
+  for (let td of document.querySelectorAll('td')) {
+    if (+(td.textContent) < 18) {
+      td.style.textDecoration = 'line-through';
+    }
+  }
+};
+
+
